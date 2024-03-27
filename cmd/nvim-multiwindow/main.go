eabdc7eacb18b2b214ac532d0c06437b462242d0
mihaimiuta <mihaimiuta@proton.me>
2024-03-27 13:55:22 +0200

    [master] Get command running

--- cmd/nvim-multiwindow/main.go
+++ cmd/nvim-multiwindow/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	ps "github.com/mitchellh/go-ps"
@@ -15,30 +16,44 @@ const (
 	WezTerm Terminal = "wezterm"
 )
 
-func getRunningTerminal() (ps.Process, error) {
+func getRunningTerminal() (Terminal, error) {
 	pid := os.Getpid()
 
 	for range 5 {
 		process, err := ps.FindProcess(pid)
 
 		if err != nil {
-			return nil, err
+			return "", err
 		}
 
 		parentProcess, err := ps.FindProcess(process.PPid())
 
 		if err != nil {
-			return nil, err
+			return "", err
 		}
 
 		if strings.Contains(parentProcess.Executable(), string(WezTerm)) {
-			return parentProcess, nil
+			return WezTerm, nil
 		}
 
 		pid = parentProcess.Pid()
 	}
 
-	return nil, errors.New("terminal not found")
+	return "", errors.New("terminal not found")
+}
+
+type CommandInput struct {
+	name string
+	args []string
+}
+
+func getCommandInput(terminal Terminal) (CommandInput, error) {
+	switch terminal {
+	case WezTerm:
+		return CommandInput{name: "/opt/homebrew/bin/wezterm-gui", args: []string{"start"}}, nil
+	default:
+		return CommandInput{}, errors.New("unknown terminal passed")
+	}
 }
 
 func main() {
@@ -48,5 +63,25 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Terminal: %s\n\twith pid:%d\n", terminal.Executable(), terminal.Pid())
+	commandInput, err := getCommandInput(terminal)
+
+	if err != nil {
+		panic(err)
+	}
+
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	args := append(commandInput.args, "nvim", cwd)
+
+	pula := exec.Command(commandInput.name, args...)
+	output, startError := pula.CombinedOutput()
+	fmt.Printf("%+v", string(output))
+
+	if startError != nil {
+		panic(startError)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module mihaimiuta/nvim-multiwindow
 
 go 1.22.0
 
-require github.com/mitchellh/go-ps v1.0.0 // indirect
+require github.com/mitchellh/go-ps v1.0.0
